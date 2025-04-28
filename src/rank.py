0174f77045d0d46339d913447dac832c5f7e1383
AveryJD <averyjdoiron@icloud.com>
2025-04-27 21:56:58 -0300

    Add rank.py
    The script that makes player rankings

---
+++ src/rank.py
@@ -0,0 +1,19 @@
+# ====================================================================================================
+# SCRIPT TO GENERATE NHL PLAYER ATTRIBUTE RANKINGS
+# ====================================================================================================
+
+import sys
+DATA_DIR = '/Users/averyjdoiron/Documents/GitHub/NHL-Player-Stat-Cards'
+sys.path.append(DATA_DIR)
+
+# Imports
+from src.utils import rank_generation as rank
+from src.utils import constants
+
+
+# Main loop to iterate through seasons and make player rankings
+for season in constants.SEASONS:
+    rank.make_skater_rankings(season)
+    rank.make_goalie_rankings(season)
+
+    
