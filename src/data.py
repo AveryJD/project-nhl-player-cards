74fffd7d7271cca9e77cf9ac2f9de827c9c63060
AveryJD <averyjdoiron@icloud.com>
2025-04-27 21:56:03 -0300

    Add data.py
    The script that scrapes NHL player stats and profiles

---
+++ src/data.py
@@ -0,0 +1,21 @@
+# ====================================================================================================
+# SCRIPT TO SCRAPE NHL PLAYER PROFILES AND STATS DATA
+# ====================================================================================================
+
+import sys
+DATA_DIR = '/Users/averyjdoiron/Documents/GitHub/NHL-Player-Stat-Cards'
+sys.path.append(DATA_DIR)
+
+# Imports
+from src.utils import data_generation as data
+from src.utils import constants
+
+
+# Main loop to iterate through seasons and situations and  get data
+for season in constants.SEASONS:
+    data.get_skater_profiles(season)
+    data.get_goalie_profiles(season)
+    for situation in constants.SITUATIONS:
+        data.get_skater_stats(season, situation)
+        data.get_goalie_stats(season, situation)
+
