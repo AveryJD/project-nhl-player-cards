c58e233e79eb57ad03286310e637653834401376
AveryJD <averyjdoiron@icloud.com>
2025-07-10 00:49:38 -0300

    Update script

--- src/data.py
+++ src/data.py
@@ -4,41 +4,16 @@
 
 # Imports
 from utils import data_generation as dg
-from utils import data_cleaning as dc
 from utils import constants
 
 
 for season in constants.SEASONS:
-
-    for pos in constants.POSITIONS:
-        
-        # Scrape & save bios
-        s_bio_url = dg.make_nst_url(season, situation='all', stdoi='bio', position=pos)
-        s_bio_df = dg.scrape_data(s_bio_url)
-        cleaned_s_bio_df = dc.clean_dataframe(s_bio_df)
-        dg.save_data_csv(cleaned_s_bio_df, folder='bios', filename=f'{season}_{pos}_bios.csv')
-
-        if pos != 'G':
-            # Scrape & save skater stats
-            for sit in constants.SKATER_SITUATIONS:
-                std_url = dg.make_nst_url(season, situation=sit, stdoi='std', position=pos)
-                std_df = dg.scrape_data(std_url)
-
-                oi_url = dg.make_nst_url(season, situation=sit, stdoi='oi', position=pos)
-                oi_df = dg.scrape_data(oi_url)
-
-                merge_keys = ['Player', 'Team', 'Position', 'GP', 'TOI']
-                stats_df = dg.merge_data(std_df, oi_df, merge_keys)
-                cleaned_stats_df = dc.clean_dataframe(stats_df)
-
-                dg.save_data_csv(cleaned_stats_df, folder='stats', filename=f'{season}_{pos}_{sit}_stats.csv')
-
+    for position in constants.POSITIONS:
+        dg.scrape_and_save_bios(season, position)
+        if position != 'G':
+            for situation in constants.SKATER_SITUATIONS:
+                dg.scrape_and_save_stats(season, position, situation)
         else:
-            # Scrape & save goalie stats
-            for sit in constants.GOALIE_SITUATIONS:
-                g_stats_url = dg.make_nst_url(season, situation=sit, stdoi='g', position='G')
-                g_stats_df = dg.scrape_data(g_stats_url)
-
-                cleaned_g_stats_df = dc.clean_dataframe(g_stats_df)
-                dg.save_data_csv(cleaned_g_stats_df, folder='stats', filename=f'{season}_G_{sit}_stats.csv')
+            for situation in constants.GOALIE_SITUATIONS:
+                dg.scrape_and_save_stats(season, position, situation)
 
--- src/rank.py
+++ src/rank.py
@@ -7,8 +7,7 @@ from utils import rank_generation as rank
 from utils import constants
 
 
-# Main loop to iterate through seasons and make player rankings
 for season in constants.SEASONS:
-    rank.make_skater_rankings(season)
-    rank.make_goalie_rankings(season)
+    for pos in constants.POSITIONS:
+        rank.make_player_rankings(season, pos)
 
