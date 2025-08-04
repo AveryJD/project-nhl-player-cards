b266ad28a6a835f500b90c306271e50c76bbb406
AveryJD <averyjdoiron@icloud.com>
2025-08-03 22:20:43 -0300

    Update cards.py

--- src/cards.py
+++ src/cards.py
@@ -14,10 +14,10 @@ card.make_player_card('Sidney Crosby', '2024-2025', 'f')
 """
 Different functions for generating player cards:
 
-card.make_player_card('Sidney Crosby', '2024-2025', 'f')
+card.make_player_card('Sidney Crosby', '2024-2025', 'F')
 card.make_team_player_cards('TOR', '2024-2025')
-card.make_position_player_cards('2024-2025', 'g')
-card.make_rank_player_cards('off', '2024-2025', 'd')
+card.make_position_player_cards('2024-2025', 'G')
+card.make_rank_player_cards('off', '2024-2025', 'D')
 card.make_all_player_cards('2024-2025')
-card.make_specific_player_cards(constants.F_PLAYERS, '2024-2025', 'f')
+card.make_specific_player_cards(constants.F_PLAYERS, '2024-2025', 'F')
 """
