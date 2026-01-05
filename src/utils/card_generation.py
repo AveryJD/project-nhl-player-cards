dd8f006336a87a7212e0041ceb528a249e90bc38
AveryJD <averyjdoiron@icloud.com>
2026-01-05 00:52:20 -0400

    Remove unnecessary card generation functions

--- src/utils/card_generation.py
+++ src/utils/card_generation.py
@@ -23,20 +23,6 @@ def make_player_card(player_name: str, season: str, pos: str, mode: str = 'light
     cf.make_player_card(player_name, season, pos, mode=mode)
 
 
-def make_specific_player_cards(specific_players: list, cur_season: str, pos: str, mode: str = 'light') -> None:
-    """
-    Generate player cards for a list of specific players.
-
-    :param specific_players: A list of str player names ('First Last')
-    :param cur_season: A str representing the season ('YYYY-YYYY')
-    :param pos: A str representing the players' position ('F', 'D', or 'G')
-    :param mode: A str determining the style of card ('light' or 'dark')
-    :return: None
-    """
-    for player_name in specific_players:
-        cf.make_player_card(player_name, cur_season, pos, mode=mode)
-
-
 def make_team_player_cards(team: str, season: str, mode: str = 'light') -> None:
     """
     Generate player cards for every player on a specific team during a given season.
@@ -54,7 +40,7 @@ def make_team_player_cards(team: str, season: str, mode: str = 'light') -> None:
                 cf.make_player_card(player_name, season, pos, mode=mode)
 
 
-def make_position_player_cards(season: str, pos: str, mode: str = 'light') -> None:
+def make_all_position_player_cards(season: str, pos: str, mode: str = 'light') -> None:
     """
     Generate player cards for all players of a given position during a specific season.
 
@@ -84,19 +70,3 @@ def make_all_player_cards(season: str, mode: str = 'light') -> None:
             cf.make_player_card(player_name, season, pos, mode=mode)
 
 
-def make_rank_player_cards(rank: str, season: str, pos: str, mode: str = 'light') -> None:
-    """
-    Generate player cards for all players of a specific position, sorted by a given attribute rank.
-
-    :param rank: A str of the attribute name to sort by (e.g. 'evo')
-    :param season: A str representing the season ('YYYY-YYYY')
-    :param pos: A str representing the position ('F', 'D', or 'G')
-    :param mode: A str determining the style of card ('light' or 'dark')
-    :return: None
-    """
-    cur_season_data = file.load_card_data_csv(season, pos)
-    cur_season_data = cur_season_data.sort_values(by=f'{rank}_rank', ascending=True)
-
-    for _, player_row in cur_season_data.iterrows():
-        player_name = player_row['Player']
-        cf.make_player_card(player_name, season, pos, mode=mode)
